a99efa2241d724ed09b73f3428ee3474fa7be7a6
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2019-05-31 10:49:17 +0100

    First commit

---
+++ src/ForLoops.cpp
@@ -0,0 +1,22 @@
+/*
+ * ForLoops.cpp
+ *
+ *  Created on: 28 May 2019
+ *      Author: Dave
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	for(int i = 0; i < 10; i ++)
+	{
+		cout << "This is a for loop on loop number: " << (i + 1) << endl;
+	}
+
+	return 0;
+}
+
+
